ff07e553e12a59461b2a9d6125a7c5a1721f63dd
pratapadarsh06 <84059028+pratapadarsh06@users.noreply.github.com>
2021-11-10 15:21:42 +0530

    update news.js
    updated new API

--- Node.js, Express.js/News-website/src/routes/news.js
+++ Node.js, Express.js/News-website/src/routes/news.js
@@ -5,13 +5,15 @@ const axios = require('axios') //axios helps fetching data from blogs
 newsRouter.get('', async(req, res)=>{
 	
     try {
-       const newsAPI = await axios.get('https://raddy.co.uk/wp-json/wp/v2/posts/')      
-       //console.log(newsAPI.data)
+       const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7169ea21ec1043c18ef41d94a166d805')      
+       //const newsAPI = await axios.get('https://raddy.co.uk/wp-json/wp/v2/posts/')      
+    
+       console.log(newsAPI.data)
        res.render('news',{articles: newsAPI.data}) 
     }catch(err){
     	if(err.response){
     		res.render('news',{articles: null})
-         console.log(err.response.data)
+            console.log(err.response.data)
     		console.log(err.response.status)
     		console.log(err.response.headers)
     	}else if (err.requiest){
@@ -27,33 +29,33 @@ newsRouter.get('', async(req, res)=>{
 })
 
 
-newsRouter.get('/:id', async(req, res) => {
-    let articleID = req.params.id
+// newsRouter.get('/:id', async(req, res) => {
+//     let articleID = req.params.id
 
-    try {
-        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/${articleID}`)
-        res.render('newsSingle', { article : newsAPI.data })
-    } catch (err) {
-        if(err.response) {
-            res.render('newsSingle', { article : null })
-            console.log(err.response.data)
-            console.log(err.response.status)
-            console.log(err.response.headers)
-        } else if(err.requiest) {
-            res.render('newsSingle', { article : null })
-            console.log(err.requiest)
-        } else {
-            res.render('newsSingle', { article : null })
-            console.error('Error', err.message)
-        }
-    } 
-})
+//     try {
+//         const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${articleID}&apiKey=7169ea21ec1043c18ef41d94a166d805`)    
+//         res.render('newsSingle', { article : newsAPI.data })
+//     } catch (err) {
+//         if(err.response) {
+//             res.render('newsSingle', { article : null })
+//             console.log(err.response.data)
+//             console.log(err.response.status)
+//             console.log(err.response.headers)
+//         } else if(err.requiest) {
+//             res.render('newsSingle', { article : null })
+//             console.log(err.requiest)
+//         } else {
+//             res.render('newsSingle', { article : null })
+//             console.error('Error', err.message)
+//         }
+//     } 
+// })
 
 
 newsRouter.post('', async(req, res) => {
     let search = req.body.search
     try {
-        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts?search=search`)
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=7169ea21ec1043c18ef41d94a166d805`)
         res.render('newsSearch', { articles : newsAPI.data })
     } catch (err) {
         if(err.response) {
@@ -81,4 +83,4 @@ module.exports = newsRouter
 
 
 
-//7169ea21ec1043c18ef41d94a166d805
+//https://newsapi.org/v2/everything?q=tesla&from=2021-09-20&sortBy=publishedAt&apiKey=7169ea21ec1043c18ef41d94a166d805
